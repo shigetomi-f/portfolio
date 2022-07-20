7a9b77dc0be8a389843622a0b5f7da45b4c61c80
miech <106013590+shigetomi-f@users.noreply.github.com>
2022-07-20 22:52:56 +0900

    20220720

--- Demo-Event/js/script.js
+++ Demo-Event/js/script.js
@@ -63,8 +63,139 @@ $(function () {
   })
 });
 
+// テスト
+
+// window.onload = function() {
+
+//   var address, subject, body, hiddenData;
+//   var sendmail = document.getElementById('mail_btn');
+
+//   sendmail.onclick = function() {
+
+//       txtData = document.getElementById('txt_data').value;
+//       address1 = 'メールアドレス';
+//       address2 = '@co.jp';
+//       address = address1+address2;
+//       subject = '○○の件について';
+//       body = 'メール本文' + '%0D%0A' + txtData; 
+
+//       location.href = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
+//   };
+
+// };
+// テスト
+
+// 画面で入力してメール起動
+window.onload = function() {
+
+  var address, subject, nam, price, body, hiddenData;
+  var sendmail = document.getElementById('mail_btn');
+
+  sendmail.onclick = function() {
+
+      txtData = document.getElementById('txt_data').value;
+      address1 = 'メールアドレス';
+      address2 = '@co.jp';
+      address = address1+address2;
+      subject = '○○の件について';
+      // body = 'メール本文' 
+      // + '%0D%0A' + txtData  + 'メール本文'; 
+      nam = parseInt(txtData, 10);
+      price = nam * 780 ;
+      all = 300 + price ;
+      body = `この度は【MAISON MARLO】をご利用いただき
+          誠にありがとうございました。`
+      + 'ご注文いただきました商品の在庫を確認いたしましたので、'
+      + 'お手数かけますが、こちらのメールに送り先住所をご記入のうえ'
+      + '返送していただくようお願いいたします。'
+      + 'お客様からのご入金確認後、即発送させていただきます。'
+      + '※ご入金は別途送信させていただきます、指定口座へお願いします。'
+      + '本日より7日以内にご入金いただけない場合は、自動的にキャンセルと'
+      + 'させて頂きます。'
+      + 'ご了承くださいませ。'
+      + ''
+      + '----------------------------------------------------------------'
+      + '［　商　品　詳　細　］'
+      + '----------------------------------------------------------------'
+      + '【　商　品　番　号　】002093-1'
+      + '【　商　　品　　名　】BAMLE sumi-kuro'
+      + '【　価　格　(税込)　】' + price + '円'
+      + '【　　数　　　量　　】'
+      + '%0D%0A' + nam  
+      + '【　　送　　　料　　】300円'
+      + '【　　合　　　計　　】' + all + '円'
+      + ''
+      + '  ~~~~~~~~~~~~~~~~~以下、お客様記入欄~~~~~~~~~~~~~~~~~'
+      + '  ▼配送先情報'
+      + ' ==============================================='
+      + '  【　お　　名　　前　】         '
+      + '  【　郵　便　番　号　】'
+      + '  【　ご　　住　　所　】'
+      + '  【　電　話　番　号　】'
+      + '  ==============================================='
+      + '  '
+      + '  今後とも【MAISON MARLO】を宜しくお願いいたします。'
+      + '  ****************************************************************'
+      + '  【MAISON MARLO】'
+      + '  所在地：福岡県福岡市博多区住吉１丁目１Yufo Garden F1'
+      + '  mail:XXX@shop-pro.jp'
+      + '  ****************************************************************' ; 
+
+      location.href = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
+  };
+
+};
 
 
+// 画面で入力してメール起動
+// window.onload = function() {
+
+//   var address, subject, body, hiddenData;
+//   var sendmail = document.getElementById('mail_btn');
+
+//   sendmail.onclick = function() {
+
+//       txtData = document.getElementById('txt_data').value;
+//       address1 = 'メールアドレス';
+//       address2 = '@co.jp';
+//       address = address1+address2;
+//       subject = '○○の件について';
+//       // body = 'メール本文' 
+//       // + '%0D%0A' + txtData  + 'メール本文'; 
+//       body = `お問い合わせありがとうございます。`
+//       + '  ****************************************************************'
+//       + ' お問い合わせ内容記入欄：'
+//       + '  ****************************************************************' ; 
+
+
+//       location.href = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
+//   };
+
+// };
+
+
+
+
+// テストお問い合わせフォーム
+
+// window.onload = function() {
+
+//   var address, subject, body, hiddenData;
+//   var sendmail = document.getElementById('mail_btn');
+
+//   sendmail.onclick = function() {
+
+//       txtData = document.getElementById('txt_data').value;
+//       address1 = 'メールアドレス';
+//       address2 = '@co.jp';
+//       address = address1+address2;
+//       subject = '○○の件について';
+//       body = 'メール本文' + '%0D%0A' + txtData; 
+
+//       location.href = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
+//   };
+
+// };
 
 
 
